499768ffb18f9c42bf5961830596d08a41a7e5d3
Maarten Menten <mentenmaarten@gmail.com>
2019-04-27 01:13:01 +0200

    Update uninstall.php
    - let widgets api delete widget data

--- uninstall.php
+++ uninstall.php
@@ -7,27 +7,8 @@ defined( 'WP_UNINSTALL_PLUGIN' ) or exit;
 require_once dirname( __FILE__ ) . '/includes/widgets.php';
 
 // Remove options
-
 delete_option( 'wdc_version' );
+delete_option( 'wdc_update_from' );
 
 // Remove widget conditions
-
-$sidebars_widgets = get_option( 'sidebars_widgets' );
-
-if ( is_array( $sidebars_widgets ) ) 
-{
-	if ( isset( $sidebars_widgets['array_version'] ) ) 
-	{
-		unset( $sidebars_widgets['array_version'] );
-	}
-
-	foreach ( $sidebars_widgets as $widgets ) 
-	{
-		if ( ! is_array( $widgets ) ) continue;
-
-		foreach ( $widgets as $widget_id ) 
-		{
-			delete_widget_conditions( $widget_id );
-		}
-	}
-}
+delete_widgets_conditions();
